43c61dff73196f63b236f03027bd685213259785
Muhammad Asyraf <acapxasyrafx#yahoo.com>
2019-12-11 17:11:05 +0800

    NewUpdate on FDG-MM

---
+++ dataSn.js
@@ -0,0 +1,129 @@
+var UserSn;
+var name;
+var link;
+
+function checkuserSn() {
+
+	if ($user[1] == 1017) {
+
+		name = "Muhammad Faris"
+		return
+	} else if ($user[1] == 1014) {
+
+		name = "Hani Izzati"
+		return
+	} else if ($user[1] == 1009) {
+
+		name = "Norwizana";
+		return
+	}
+
+	else if ($user[1] == 2) {
+
+		name = "ADMIN";
+		return
+	}
+
+	else if ($user[1] == 1007) {
+
+		name = "Siti Norfatiha";
+		return
+	}
+
+	else if ($user[1] == 1040) {
+
+		name = "Nur Izzati";
+		return
+	}
+
+	else if ($user[1] == 1037) {
+
+		name = "Muhammad Hafiz";
+		return
+	}
+
+	else if ($user[1] == 1001) {
+
+		name = "Dr. Abdul Rahman";
+		return
+	}
+
+	else if ($user[1] == 1029) {
+
+		name = "Mohd Hazizi";
+		return
+	}
+
+	else if ($user[1] == 1011) {
+
+		name = "Adi Effendi";
+		return
+	}
+
+	else if ($user[1] == 1033) {
+
+		name = "Muhammad Asyraf";
+		link = "https://outlook.office365.com/owa/calendar/0af28dc93693442d80e141b1a2d1c3a1@araken.biz/71d635d1116649c3bc2c5df758bec98f7802203314192688606/calendar.ics";
+		return
+	}
+
+	else if ($user[1] == 1006) {
+
+		name = "Muhammad Nur Azizi";
+		return
+	}
+
+	else if ($user[1] == 1005) {
+
+		name = "Nurul Saadah";
+		return
+	}
+
+	else if ($user[1] == 1020) {
+
+		name = "Amirul Aiman";
+		return
+	}
+
+	else if ($user[1] == 1030) {
+
+		name = "Khoshim";
+		return
+	}
+
+	else if ($user[1] == 1002) {
+
+		name = "Sharifah Aishah";
+		return
+	}
+
+	else if ($user[1] == 1027) {
+
+		name = "Khairinah Izzah";
+		return
+	}
+
+	else if ($user[1] == 1016) {
+
+		name = "Azmi";
+		return
+	}
+
+	else if ($user[1] == 1034) {
+
+		name = "Muhammad Zulkhairi";
+		return
+	}
+
+	else if ($user[1] == 1021) {
+
+		name = "Asyira";
+		return
+	}
+	else {
+		dataErr = error.checkuserSn
+		return dataErr
+	}
+
+
+}
